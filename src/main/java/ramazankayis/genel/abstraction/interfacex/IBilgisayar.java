23b23574148b1ed4dd6ef476e96e5391afc2648a
ramazankayis <kayis.ramazan@gmail.com>
2022-12-11 15:49:33 +0300

    interface structure

---
+++ src/main/java/ramazankayis/genel/abstraction/interfacex/IBilgisayar.java
@@ -0,0 +1,11 @@
+package ramazankayis.genel.abstraction.interfacex;
+
+import java.util.Date;
+
+public interface IBilgisayar {
+      String bilgisayarAdi();
+      String bilgisayarFiyati();
+
+      public void bilgisayarUretim(Date tarih);
+
+}
---
+++ src/main/java/ramazankayis/genel/abstraction/interfacex/Msi.java
@@ -0,0 +1,20 @@
+package ramazankayis.genel.abstraction.interfacex;
+
+import java.util.Date;
+
+public class Msi implements IBilgisayar{
+    @Override
+    public String bilgisayarAdi() {
+        return null;
+    }
+
+    @Override
+    public String bilgisayarFiyati() {
+        return null;
+    }
+
+    @Override
+    public void bilgisayarUretim(Date tarih) {
+
+    }
+}
