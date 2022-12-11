ca8e7e7bb5646ec92154e888f1978673c595f2b8
ramazankayis <kayis.ramazan@gmail.com>
2022-12-11 15:22:04 +0300

    polymorphism structure

--- src/main/java/ramazankayis/genel/inheritance/InheritanceMainTest.java
+++ src/main/java/ramazankayis/genel/inheritance/InheritanceMainTest.java
@@ -3,9 +3,12 @@ package ramazankayis.genel.inheritance;
 public class InheritanceMainTest {
     public static void main(String[] args) {
 
-        Ramazan ramazan = new Ramazan();
+        /*Ramazan ramazan = new Ramazan();
         ramazan.setAdi("ramazan");
 
-        System.out.println(ramazan.getAdi());
+        System.out.println(ramazan.getAdi());*/
+
+        Person ramazan= new Ramazan();
+        Person kayis= new Kayis();
     }
 }
