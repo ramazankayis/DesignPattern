f6a9de5f80bf83628c2b43d6ba88e6c75ba97ea3
ramazankayis <kayis.ramazan@gmail.com>
2022-12-11 23:47:10 +0300

    yazılım prensibleri

---
+++ src/main/java/ramazankayis/genel/yazılımprensibleri/Prensibler.java
@@ -0,0 +1,13 @@
+package ramazankayis.genel.yazılımprensibleri;
+
+public class Prensibler {
+//clean code
+    //DRY : Don't repeat yourself
+    //KISS: Keep it simple stupid
+    //YANGI : you ain't gonna need it
+
+    //CCP:
+    //REP
+
+
+}
